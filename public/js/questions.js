427d51d08e90bff1c94ac8740a22172b691f7a3f
henry le <henryle@henrys-MacBook-Pro.local>
2015-04-07 18:38:45 -0700

    final version

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,7 +1,5 @@
 // Client-side code
 /* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
-
-
  	$(document).ready(function() {
  		"use strict";
  		//handlebar compiler
